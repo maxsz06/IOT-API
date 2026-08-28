69f808835527d9f57d0c48cef50851e5e0c0ce5c
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2026-08-28 09:14:35 -0300

    API & INTEGRAÇÃO

--- routes/iot_routes.js
+++ routes/iot_routes.js
@@ -3,16 +3,33 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const bodyParserJSON = bodyParser.json()
 
+// Para criar a cominicação entre a API e o Equipamento IoT, é necessario instalar
+// a depedencia mqtt => npm install mqtt --save
+const mqtt = require('mqtt') // Import da biblioteca apos a instalação
+
+//Criando um cliente prara se comunicar com o sevidor MQTT 
+//através do protocolo mqtt
+const mqttClient = mqtt.connect('mqtt://broker.hivemq.com')
+
+
 router.post('/led', bodyParserJSON, async function (request, response) {
     let dados = request.body
-    let contentType = request.headers['content-type']
 
-    console.log('Dados recebidos:', dados)
-    console.log('Content-Type:', contentType)
+    if (dados.comando === 'ligar') {
+        mqttClient.publish('senaijandira/sala/manha/25203648', 'ligar')
+    } else if (dados.comando === 'desligar') {
+        mqttClient.publish('senaijandira/sala/manha/25203648', 'desligar')
+    } else {
+        return response.status(400).json({
+            status_code: 400,
+            message: 'Comando Invalido',
+            dados_recebidos: dados
+        })
+    }
 
     response.status(200).json({
         status_code: 200,
-        message: 'Recebido com sucesso',
+        message: 'Comando enviado com sucesso',
         dados_recebidos: dados
     })
 })
